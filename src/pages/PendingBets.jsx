4fd881893df57a937f7d6a999c0a04fe42ca5111
Aniket Bamotra <aniketbamotra@gmail.com>
2022-08-17 00:49:29 +0530

    pending bets

--- src/pages/PendingBets.jsx
+++ src/pages/PendingBets.jsx
@@ -6,19 +6,44 @@ import { useState } from "react";
 
 const PendingBets = () => {
   const [walletAdd, setWalletAdd] = useState("");
+  const [pendingData, setPendingData] = useState();
+
   const getPendingBets = async () => {
     const res = await axios.get(
       `https://usdcpending.mbdqwfss.repl.co/?key=${walletAdd}`
     );
     console.log(res);
+    setPendingData(res.data);
     console.log(walletAdd);
   };
+  if (walletAdd == "")
+    return (
+      <div>
+        <Nav setWalletAdd={setWalletAdd} />
+        <h3 className="serif-600">Pending Bets.</h3>
+        <h5 className="serif-400">
+          Please connect your wallet and click the button bellow.
+        </h5>
+      </div>
+    );
+
   return (
     <div>
       <Nav setWalletAdd={setWalletAdd} />
-      <h3 className="serif">Pending Bets.</h3>
-      <div className="p-bets-wrapper sans" onClick={getPendingBets}>
-        get bets!!
+      <div>
+        <h3 className="serif">Pending Bets.</h3>
+        <div className="p-bets-wrapper sans btn" onClick={getPendingBets}>
+          Get bets!!
+        </div>
+      </div>
+      <div>
+        <h4 className="serif-600">Event.</h4>
+        <h5 className="serif-400">Date.</h5>
+        <div className="bet-event open-bet-details">
+          <div>
+            <h4 className="serif-600 team-name home-team">Backing Home</h4>
+          </div>
+        </div>
       </div>
     </div>
   );
