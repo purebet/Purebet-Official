cd9011f3a94c0bbb9ce1e4997d098a205f2bda61
lbianlbian <83378530+lbianlbian@users.noreply.github.com>
2022-09-19 13:39:51 -0700

    Update Exchange.jsx

--- src/pages/Exchange.jsx
+++ src/pages/Exchange.jsx
@@ -6,6 +6,8 @@ import bgImg from "../images/Ellipse.png";
 import AvailableBets from "../components/AvailableBets";
 import Categories from "../components/Categories";
 import BetSlip from "../components/BetSlip";
+import "@solana/web3.js";
+var solanaWeb3 = require("@solana/web3.js");
 
 const Exchange = () => {
   const [betData, setBetData] = useState([]);
@@ -22,9 +24,60 @@ const Exchange = () => {
   const [catId, setCatId] = useState(0);
   const avaiBets = async () => {
     try {
+      var connection = new solanaWeb3.Connection("https://devnet.genesysgo.net/", "confirmed");
+      var programID = new solanaWeb3.PublicKey("M8WYXm9YGPcBqt8QpAMgZXbMFjVXeTyMrQ94pAtkitK");
+      var all0s = new solanaWeb3.PublicKey(new Uint8Array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]));
+      //later this variable will be populated from the database. each id1 * 256 + id2 will have all events linked with it
+      var initJSON = {"0":{
+                      "event": "Purebet to be ready on October 20th", 
+                      "homeTeam": "Yes", 
+                      "awayTeam": "No", 
+                      "sport": "Programming, Designing, and Writing", 
+                      "date": "October 20th",
+                      "home": [ {"odds": 0, "totalAmount": 0, "accArr": [{"amount": 0, "acc": "blank"}] }, 
+                                {"odds": 0, "totalAmount": 0, "accArr": [{"amount": 0, "acc": "blank"}] }, 
+                                {"odds": 0, "totalAmount": 0, "accArr": [{"amount": 0, "acc": "blank"}] } 
+                              ], 
+                      "away": [{"odds": 0, "totalAmount": 0, "accArr": [{"amount": 0, "acc": "blank"}] },
+                               {"odds": 0, "totalAmount": 0, "accArr": [{"amount": 0, "acc": "blank"}] },
+                                {"odds": 0, "totalAmount": 0, "accArr": [{"amount": 0, "acc": "blank"}] } 
+                              ]
+                     }};
+      //home is blank so people can bet on home
+      var homeAccs = await connection.getProgramAccounts(
+        programID, 
+        {filters:
+          [
+            {memcmp: {offset: 6, bytes: all0s.toBase58()} },
+            {DataSizeFilter: {dataSize: 71} }
+          ]
+        },
+        {dataSlice: {length: 6, offset:0} }
+      );
+      
+      for(var x = 0; x < homeAccs.length; x++){
+        var id1 = accs[x].account.data[0];
+        var id2 = accs[x].account.data[1];
+        var position = id1 * 256 + id2;
+        
+        var stakeHome = (homeAccs[x].account.data[2] * 256 + accs[x].account.data[3]) / 100;
+        var stakeAway = (homeAccs[x].account.data[4] * 256 + accs[x].account.data[5]) / 100;
+        if(stakeHome == 0 || stakeAway == 0){
+          continue;
+        }
+        
+        var toMatchStake = stakeHome;
+        var toMatchOdds = (stakeHome + stakeAway) / stakeHome;
+        //var highest = initJson.outputNee.home[0];
+        //var secondHighest = output[indexNeeded].home[1];
+        //var thirdHighest = output[indexNeeded].home[2];
+        
+      
+      /*
       const res = await axios.get("https://script.google.com/macros/s/AKfycbxCY_YhtNe-GCBm98zDC11eSx6p9-cwQSfxS56BM3dCpY6cl5EbM-RU5zSn2_x3ex4HQg/exec");
       console.log(res.data);
       setAvaiOdds(res.data);
+      */
     } catch (error) {
       console.log(error);
     }
@@ -49,7 +102,8 @@ const Exchange = () => {
       <div className="betting-events-wrapper">
         <h3 className="serif-600 sect-title">Upcoming Events.</h3>
         <div className="bet-events">
-          {avaiOdds.slice(0, 4).map((item, index) => {
+          //there used to be a .slice(0, 4) after avaiOdds, but I took it out because for the time being we won't have more than 5 events.
+          {avaiOdds.map((item, index) => {
             return (
               <AvailableBets
                 setBetData={setBetData}
